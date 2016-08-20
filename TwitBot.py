ab74a5f192bf9296007c422b118dd8c3f1589f56
miket1113 <mat8367@g.rit.edu>
2016-08-20 01:53:16 -0400

    finished, base idea that is

--- TwitBot.py
+++ TwitBot.py
@@ -7,5 +7,26 @@ import os
 from markovbot import MarkovBot
 
 
+tweetbot = MarkovBot()
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+quotes = os.path.join(dir, 'Quotes.txt')
+
+tweetbot.read(quotes)
+
+first_tweet = tweetbot.generate_text(10)
+print('Tolkien: ')
+print(first_tweet)
+
+cons_key = 'ylgW0M6gI8uXrOEsLFFLnheoW'
+cons_secret = 't1XmD9KlRcllWQwXHFMASxSJZ7eKKNV7kmdn9tqv05lkoT17mN'
+access_token = '766721464917712896-i5YcQCcgbgLD2K1PidTyenPjnD3L1wR'
+access_token_secret = 'tdx74a4fRf7crogczxreIphFHqov2bmhIQPMjB3lwsEs0'
+
+tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
+
+
+tweetbot.twitter_tweeting_start(days=1, hours=0, minutes=0, keywords=None, prefix=None, suffix='#PyGaze')
 
 
